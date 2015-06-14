245a3ca517edc4c66b7d12c5ab7ac18d478a12b9
yuxuanchiadm <yuxuanchiadm@126.com>
2015-06-14 08:40:23 +0800

    Add generic parameters to ScalarType

--- src/main/java/org/apcdevpowered/vcpu32/vm/storage/NodeScalar.java
+++ src/main/java/org/apcdevpowered/vcpu32/vm/storage/NodeScalar.java
@@ -1,5 +1,19 @@
 package org.apcdevpowered.vcpu32.vm.storage;
 
-public final class NodeScalar extends NodeElement
+public class NodeScalar<S extends NodeScalar<S>> extends NodeElement
 {
+    private final ScalarType<S> type;
+    
+    private NodeScalar(ScalarType<S> type)
+    {
+        this.type = type;
+    }
+    public ScalarType<S> getType()
+    {
+        return type;
+    }
+    public <T extends ScalarType<S>> T getType(Class<T> clazz)
+    {
+        return clazz.cast(type);
+    }
 }
--- src/main/java/org/apcdevpowered/vcpu32/vm/storage/ScalarType.java
+++ src/main/java/org/apcdevpowered/vcpu32/vm/storage/ScalarType.java
@@ -1,13 +1,14 @@
 package org.apcdevpowered.vcpu32.vm.storage;
 
-public abstract class ScalarType
+public abstract class ScalarType<S extends NodeScalar<S>>
 {
-    public static abstract class ScalarTypeNumber extends ScalarType
+    public static abstract class ScalarTypeNumber<S extends NodeScalar<S>> extends ScalarType<S>
     {
         private final int byteCount;
         
-        public ScalarTypeNumber(int byteCount)
+        public ScalarTypeNumber(Class<S> clazz, int byteCount)
         {
+            super(clazz);
             this.byteCount = byteCount;
         }
         public int getByteCount()
@@ -15,31 +16,32 @@ public abstract class ScalarType
             return byteCount;
         }
     }
-    public static class ScalarTypeNaturalNumber extends ScalarTypeNumber
+    public static class ScalarTypeNaturalNumber<S extends NodeScalar<S>> extends ScalarTypeNumber<S>
     {
-        private ScalarTypeNaturalNumber(int byteCount)
+        private ScalarTypeNaturalNumber(Class<S> clazz, int byteCount)
         {
-            super(byteCount);
+            super(clazz, byteCount);
         }
     }
-    public static class ScalarTypeRealNumber extends ScalarTypeNumber
+    public static class ScalarTypeRealNumber<S extends NodeScalar<S>> extends ScalarTypeNumber<S>
     {
-        private ScalarTypeRealNumber(int byteCount)
+        private ScalarTypeRealNumber(Class<S> clazz, int byteCount)
         {
-            super(byteCount);
+            super(clazz, byteCount);
         }
     }
-    public static class ScalarTypeNaturalNumberArray extends ScalarTypeNaturalNumber
+    public static class ScalarTypeNaturalNumberArray<S extends NodeScalar<S>> extends ScalarTypeNaturalNumber<S>
     {
-        private ScalarTypeNaturalNumberArray(int byteCount)
+        private ScalarTypeNaturalNumberArray(Class<S> clazz, int byteCount)
         {
-            super(byteCount);
+            super(clazz, byteCount);
         }
     }
-    public static class ScalarTypeString extends ScalarType
+    public static class ScalarTypeString<S extends NodeScalar<S>> extends ScalarType<S>
     {
-        private ScalarTypeString()
+        private ScalarTypeString(Class<S> clazz)
         {
+            super(clazz);
         }
     }
     
@@ -52,6 +54,13 @@ public abstract class ScalarType
     public static final ScalarTypeNaturalNumberArray SCALAR_TYPE_BYTE_ARRAY;
     public static final ScalarTypeNaturalNumberArray SCALAR_TYPE_INTEGER_ARRAY;
     public static final ScalarTypeString SCALAR_TYPE_STRING;
+    private final Class<S> clazz;
+    
+    private ScalarType(Class<S> clazz)
+    {
+        this.clazz = clazz;
+    }
+    
     static
     {
         SCALAR_TYPE_BYTE = new ScalarTypeNaturalNumber(1);
@@ -63,9 +72,5 @@ public abstract class ScalarType
         SCALAR_TYPE_BYTE_ARRAY = new ScalarTypeNaturalNumberArray(1);
         SCALAR_TYPE_INTEGER_ARRAY = new ScalarTypeNaturalNumberArray(4);
         SCALAR_TYPE_STRING = new ScalarTypeString();
-    }
-    
-    private ScalarType()
-    {
     }
 }
