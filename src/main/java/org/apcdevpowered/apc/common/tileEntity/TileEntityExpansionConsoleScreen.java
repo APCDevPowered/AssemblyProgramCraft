a1106daa16219fbb4f7e62cb8f600f4a86f90b41
yuxuanchiadm <yuxuanchiadm@126.com>
2016-07-02 12:00:37 +0800

    Fix TileEntityExpansionConsoleScreen sometimes not render

--- src/main/java/org/apcdevpowered/apc/common/tileEntity/TileEntityExpansionConsoleScreen.java
+++ src/main/java/org/apcdevpowered/apc/common/tileEntity/TileEntityExpansionConsoleScreen.java
@@ -1,7 +1,25 @@
 package org.apcdevpowered.apc.common.tileEntity;
 
+import org.apcdevpowered.apc.common.init.AssemblyProgramCraftBlocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityExpansionConsoleScreen extends TileEntity
 {
+    @Override
+    public Packet getDescriptionPacket()
+    {
+        NBTTagCompound var1 = new NBTTagCompound();
+        return new S35PacketUpdateTileEntity(this.getPos(), Block.getIdFromBlock(AssemblyProgramCraftBlocks.block_vcpu_32_computer), var1);
+    }
+    @Override
+    public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt)
+    {
+        
+    }
 }
